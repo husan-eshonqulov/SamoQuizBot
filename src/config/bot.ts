2cba09ae459cb497425c2d0fc1c14e342c0b51b4
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-03-01 19:56:36 +0500

    project spread modules

---
+++ src/config/bot.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+import { Config } from '../types/config';
+
+dotenv.config();
+
+const config: Config = {
+  botToken: process.env.BOT_TOKEN!,
+};
+
+export default config;
