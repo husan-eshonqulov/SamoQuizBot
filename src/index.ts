bfd7e05930fd0ee016da06adec8a7840797bc5be
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-03-20 22:56:20 +0500

    add some logs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import connectDb from './utils/connectDb';
-import config from './config';
 import bot from './bot';
+import config from './config';
+import connectDb from './utils/connectDb';
+import logger from './services/logger';
 
 const bootstrap = async () => {
   try {
     await connectDb(config.mongoUri);
-    bot.start();
+    logger.info('MongoDb has been connected...');
+    bot.start({
+      onStart: () => {
+        logger.info(`https://t.me/${bot.botInfo.username} has been started...`);
+      },
+    });
   } catch (err) {
-    throw err;
+    logger.error(err);
   }
 };
 
